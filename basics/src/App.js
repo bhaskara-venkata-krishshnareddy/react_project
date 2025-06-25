e5058073899adf33ceb5d76d693a82fd9359974f
Bhaskar <bhaska972@gmail.com>
2025-06-25 12:28:05 +0530

    soem changes

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,7 +1,11 @@
-
+import Header from "./Header";
 function App() {
   return (
-  <h1>hello from  react project </h1>
+    <>
+    <h1>hello from  react project </h1>
+    <Header/>
+    </>
+  
   ); 
 }
 export default App;
---
+++ basics/src/Header.jsx
@@ -0,0 +1,9 @@
+function Header () {
+  return (
+    <>
+    <h1>Header component</h1>
+    <header>soome contenr</header>
+    </>
+  )
+}
+export default Header;
